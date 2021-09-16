124b26db216756eb192dc0b459364daf99040b20
Santiago Sinisterra <s.sinisterra@uniandes.edu.co>
2021-09-16 00:01:15 -0500

    Squashed commit of the following:
    commit 5dca5153080a892b65ba8facce0f9b27a1bb52ae
    Author: Santiago Sinisterra <s.sinisterra@uniandes.edu.co>
    Date:   Wed Sep 15 23:51:50 2021 -0500
    
        EntregaFinal - laboratorio 4
    
    commit a8071475625a8bb348588960973c00ad57654ec1
    Author: Santiago Sinisterra <s.sinisterra@uniandes.edu.co>
    Date:   Wed Sep 15 21:06:12 2021 -0500
    
        Laboratorio-mediciones tiempo-no-final

--- App/model.py
+++ App/model.py
@@ -28,6 +28,10 @@
 import config as cf
 from DISClib.ADT import list as lt
 from DISClib.Algorithms.Sorting import shellsort as sa
+from DISClib.Algorithms.Sorting import quicksort as sb
+from DISClib.Algorithms.Sorting import mergesort as sc
+from DISClib.Algorithms.Sorting import insertionsort as sd
+import time
 assert cf
 
 """
@@ -36,8 +40,7 @@ los mismos.
 """
 
 # Construccion de modelos
-def newcatalog():
-    listtype = 'ARRAY_LIST'
+def newcatalog(listtype):
     catalog = { 'Artists': None,
                 'Artworks': None}
     catalog['Artists'] = lt.newList(listtype) #, comparefunction = cmp_artist
@@ -47,17 +50,82 @@ def newcatalog():
 # Funciones para agregar informacion al catalogo
 
 def addArtist(catalog, artist):
-    filtered = {'ArtistID': artist['ConstituentID'], 'Artist': artist['DisplayName'], 'Nationality': artist['Nationality'], 'Birthdate': artist['BeginDate']}
-    lt.addLast(catalog['Artists'], filtered)
+    #filtered = {'ArtistID': artist['ConstituentID'], 'Artist': artist['DisplayName'], 'Nationality': artist['Nationality'], 'Birthdate': int(artist['BeginDate'])}
+    lt.addLast(catalog['Artists'], artist)
 
 def addArtwork(catalog, artwork):
-    filtered = {'ArtistID': artwork['ConstituentID'], 'ObjectID': artwork['ObjectID'], 'Title': artwork['Title'], 'Date': artwork['Date'], 'Medium': artwork['Medium'], 'Classification': artwork['Classification'], 'Dimensions': artwork['Dimensions'], 'Weight': artwork['Weight (kg)'], 'CreditLine': artwork['CreditLine']}
-    lt.addLast(catalog['Artworks'], filtered)
+    #filtered = {'ArtistID': artwork['ConstituentID'], 'ObjectID': artwork['ObjectID'], 'Title': artwork['Title'], 'Date': artwork['Date'], 'Medium': artwork['Medium'], 'Classification': artwork['Classification'], 'Dimensions': artwork['Dimensions'], 'Weight': artwork['Weight (kg)'], 'CreditLine': artwork['CreditLine']}
+    lt.addLast(catalog['Artworks'], artwork)
+
 
 # Funciones para creacion de datos
 
+
 # Funciones de consulta
+
+def getagerange(catalog, date1, date2):
+    artists = catalog["Artists"]
+    cronartists = lt.newList()
+    for men in range(date1, date2):
+        if ((men['Birthdate'] >= date1) and (men['Birthdate'] <= date2)):
+            trueartist = lt.getElement(artists, men)
+            lt.addLast(cronartists, trueartist)
+    return cronartists
+
+
+def cronartwork(catalog, number, date1, date2, sorttype):
+    if lt.size(catalog['Artworks']) >= number:
+        sublist = lt.subList((catalog['Artworks']), 1, number)
+        sublist = sublist.copy()
+        c = lt.size(sublist) - 1
+        while c > -1:
+            element = lt.getElement(sublist, c)
+
+            if element['DateAcquired'] < date1 or element['DateAcquired'] > date2:
+                lt.deleteElement(sublist, c)
+
+            c = c - 1
+        if sorttype == 'Insertion':
+            start_time = time.process_time()
+            sd.sort(sublist, cmpArtworkByDateAcquired)
+            stop_time = time.process_time()
+
+        elif sorttype == 'Merge':
+            start_time = time.process_time()
+            sc.sort(sublist, cmpArtworkByDateAcquired)
+            stop_time = time.process_time()
+
+        elif sorttype == 'Quick Sorts':
+            start_time = time.process_time()
+            sb.sort(sublist, cmpArtworkByDateAcquired)
+            stop_time = time.process_time()
+
+        elif sorttype == 'Shell':
+            start_time = time.process_time()
+            sa.sort(sublist, cmpArtworkByDateAcquired)
+            stop_time = time.process_time()
+
+        else:
+            start_time = time.process_time()
+            sa.sort(sublist, cmpArtworkByDateAcquired)
+            stop_time = time.process_time()
+
+    else:
+        return 'N/A'
+    elapsed_time_mseg = str((stop_time - start_time)*1000)
+    return sublist, elapsed_time_mseg
 
 # Funciones utilizadas para comparar elementos dentro de una lista
 
+def compareages(artist1, artist2):
+     return (float(artist1['Birthdate']) > float(artist2['Birthdate']))
+
+def cmpArtworkByDateAcquired(artwork1, artwork2):
+    return (artwork1['DateAcquired'] < artwork2['DateAcquired'])
+
+
+
 # Funciones de ordenamiento
+
+def cronfilter(catalog):
+    sa.sort(catalog['Artistas'], compareages)
--- App/view.py
+++ App/view.py
@@ -26,7 +26,7 @@ import sys
 import controller
 from DISClib.ADT import list as lt
 assert cf
-
+#from prettytable import PrettyTable
 
 """
 La vista se encarga de la interacción con el usuario
@@ -38,16 +38,13 @@ operación solicitada
 def printMenu():
     print("Bienvenido")
     print("1- Cargar información en el catálogo")
-    print("2- Artistas en orden cronológico")
-    print('3- Adquisiciones en orden cronológico')
-    print('4- Obras de arte de artista por técnica')
-    print('5- Obras de arte por nacionalidad de artista')
-    print('6- Transportar obras de un departamento')
-    print('0- Proponer una nueva exposición en el museo')
+    print("2- Cargar sort utilizado")
+    print('3- Adquisiciones en orden cronológico y su tiempo se ordenamiento')
+    print('0- Salir del programa')
 
 
 catalog = None
-
+sorttype = None
 """
 Menu principal
 """
@@ -55,48 +52,47 @@ while True:
     printMenu()
     inputs = input('Seleccione una opción para continuar\n')
     if int(inputs[0]) == 1:
+        tipolista = input('Elija el tipo de lista, sea ya ARRAY_LIST o LINKED_LIST:')
         print("Cargando información de los archivos ....")
-        catalog = (controller.InitCatalog())
+        catalog = (controller.InitCatalog(tipolista))
         controller.loadData(catalog)
-        print("Número de Arttistas registrados: " + str(lt.size(catalog['Artists'])))
+        print("Número de Artistas registrados: " + str(lt.size(catalog['Artists'])))
         print("Número de obras de arte registradas: " + str(lt.size(catalog['Artworks'])))
-        print("")
-        print("Primeros 3 artistas: ") 
-        print(" ")
-        print(str(catalog['Artists']['elements'][:3]))
-        print("")
-        print("Ultimos 3 artistas: ")
-        print(" ")
-        print(str(catalog['Artists']['elements'][-3:]))
-        print("")
-        print("Primeras 3 obras de arte: ")
-        print("")
-        print(str(catalog['Artworks']['elements'][:3]))
-        print("")
-        print("Ultimas 3 obras de arte: " )
-        print("")
-        print(str(catalog['Artworks']['elements'][-3:]))
+
+    
     elif int(inputs[0]) == 2:
-        anioInicial = input("Año Inicial de busqueda:")
-        anioFinal = input("Año Final de busqueda:")
+        sorttype = input("ingrese el tipo de sort que desea: Insertion, Shell, Merge o Quick Sorts: ")
 
     elif int(inputs[0]) == 3:
-        fechaInicial = input("Fecha inicial de busqueda (AAAA-MM-DD):")
-        fechaFinal = input("Fecha final de busqueda (AAAA-MM-DD):")
+        Anoinicial = str(input("Indique la fecha inicial en formato AAAA-MM-DD: "))
+        Anofinal = str(input("Indique la fecha final en formato AAAA-MM-DD: "))
+        sublistnum = int(input("Indique el tamaño de la lista: "))
+        lista = (controller.cronartworks(catalog, sublistnum, Anoinicial, Anofinal, sorttype))
+        listah = lista[0]
+        lista2 = lista[0]
+        tiempo = lista[1]
+        print('')
+        print("=============== Req No. 2 Inputs ===============")
+        print("Artworks acquired between " + str(Anoinicial) + " and " + str(Anofinal))
+        print('')
+        print('=============== Req No. 2 Answer ===============')
+        print('The MoMa acquired ' + str(lt.size(lista2)) + ' unique pieces bewteen' + str(Anoinicial) + " and " + str(Anofinal))
+        print("The first and last 3 artworks in range are...")
 
-    elif int(inputs[0]) == 4:
-        nombre = input("Nombre del Artista a buscar:")
+        #table = PrettyTable(["ObjectID", "Title", "ArtistsNames", "Medium", "Dimensions", "Date", "DateAcquired", "URL"])
 
-    elif int(inputs[0]) == 5:
-        obra = input("Las obras del Museo:")
-
-    elif int(inputs[0]) == 6:
-        departamento = input("Departamento de obras que se quiere conocer el precio estimado de transporte:")
-
-    elif int(inputs[0]) == 7:
-        anioInicial = input("Año inicial de las obras:")
-        anioFinal = input("Año final de las obras:")
-        areaDisponible = input("Area disponible para la exposicion (m^2):")
+        #table.add__row([listah[0]["ObjectID"], listah[0]["Title"], listah[0]["ArtistsNames"], listah[0]["Medium"], listah[0]["Dimensions"], listah[0]["Date"], listah[0]["DateAcquired"], listah[0]["URL"]])
+        #table.add__row([listah[1]["ObjectID"], listah[1]["Title"], listah[1]["ArtistsNames"], listah[1]["Medium"], listah[1]["Dimensions"], listah[1]["Date"], listah[1]["DateAcquired"], listah[1]["URL"]])
+        #table.add__row([listah[2]["ObjectID"], listah[2]["Title"], listah[2]["ArtistsNames"], listah[2]["Medium"], listah[2]["Dimensions"], listah[2]["Date"], listah[2]["DateAcquired"], listah[2]["URL"]])
+        #table.add__row([listah[-1]["ObjectID"], listah[-1]["Title"], listah[-1]["ArtistsNames"], listah[-1]["Medium"], listah[-1]["Dimensions"], listah[-1]["Date"], listah[-1]["DateAcquired"], listah[-2]["URL"]])
+        #table.add__row([listah[-2]["ObjectID"], listah[-2]["Title"], listah[-2]["ArtistsNames"], listah[-2]["Medium"], listah[-2]["Dimensions"], listah[-2]["Date"], listah[-2]["DateAcquired"], listah[-2]["URL"]])
+        #table.add__row([listah[-3]["ObjectID"], listah[-3]["Title"], listah[-3]["ArtistsNames"], listah[-3]["Medium"], listah[-3]["Dimensions"], listah[-3]["Date"], listah[-3]["DateAcquired"], listah[-3]["URL"]])
+        
+        #Se corrompio la libreria de Prettytable, en el reto se arreglara este inconveniente, al no ser vital para la medicion de tiempo de los sorts. Gracias por su comprension. :)
+        
+        
+        print('')
+        print('El tiempo gastado en organizar los datos es: ' + tiempo + ' milisegundos')
 
     else:
         sys.exit(0)
